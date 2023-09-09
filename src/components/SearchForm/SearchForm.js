b37b5bb182123fcd4610449c8f4da159ecb4bf17
veronikadogareva <veronika.dogareva@yandex.ru>
2023-09-09 11:33:11 +0700

    исправила форму

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,8 +4,8 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 function SearchForm() {
   return (
     <section className="search-form">
-      <form className="search-form__form">
-        <input className="search-form__input" id="search-input" type="text" name="search" placeholder="Фильм" />
+      <form className="search-form__form" name="form-search">
+        <input className="search-form__input" id="search-input" type="text" name="search" placeholder="Фильм" required/>
         <button className="search-form__button" type="submit" />
       </form>
       <FilterCheckbox />

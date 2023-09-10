564ab36887fb4f7a56ec5bdee1997cec91b024e5
veronikadogareva <veronika.dogareva@yandex.ru>
2023-09-10 11:39:32 +0700

    итоговые правки

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <footer className="footer">
       <h3 className="footer__title footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
-      <ul className="footer__container">
-        <li className="footer__item"><p className="footer__copyright footer__text">© {new Date().getFullYear()}</p></li>
-        <li className="footer__item"><nav className="footer__navigation">
+      <div className="footer__container">
+        <p className="footer__copyright footer__text">© {new Date().getFullYear()}</p>
+        <nav className="footer__navigation">
           <a className="footer__link footer__text" href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
           <a className="footer__link footer__text" href="https://github.com/">Github</a>
-        </nav></li>
-      </ul>
+        </nav>
+      </div>
     </footer>
   )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -18,7 +18,7 @@ function Navigation() {
     <nav className="navigation">
       <ul className="navigation__container">
         <li className="navigation__item"><NavLink className={({ isActive }) => `navigation__link ${location.pathname === '/' ? "navigation__link_path_main" : ""} ${isActive ? "navigation__link_active" : ""}`} to="/movies">Фильмы</NavLink></li>
-        <li className="navigation__item"><NavLink className={({ isActive }) => `navigation__link ${location.pathname === '/' ? "navigation__link_path_main" : ""} `} to="/saved-movies">Сохранённые фильмы</NavLink></li>
+        <li className="navigation__item"><Link className={`navigation__link ${location.pathname === '/' ? "navigation__link_path_main" : ""} `} to="/saved-movies">Сохранённые фильмы</Link></li>
         <li className="navigation__item"><button className={`navigation__button ${location.pathname === "/" ? "navigation__button_path_main" : ""}`} onClick={handleMenuButton}></button></li>
       </ul>
       <Link className="navigation__profile" to="/profile">Аккаунт</Link>

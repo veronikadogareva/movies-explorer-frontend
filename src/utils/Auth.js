2d4da0c24b32ab94946776e56ec6c2f22bae7e94
veronikadogareva <veronika.dogareva@yandex.ru>
2023-09-22 15:01:05 +0700

    заливаю на сервер

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'http://api.veronika.movie.nomoredomainsicu.ru';
 
 export const register = ({ email, password, name }) => {
   return fetch(`${BASE_URL}/signup`, {
